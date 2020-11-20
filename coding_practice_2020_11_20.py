3a9a149577e9b4c99ed67883c256684883676be4
gavinturtl <74620025+gavinturtl@users.noreply.github.com>
2020-11-20 13:33:58 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,17 +1,20 @@
 # 1. Favourite Colour
 colour = input("What's your favourite colour? ")
 print(f"Cool! My favourite colour is {colour} too!")
+print()
 
 # 2. Packs of Cans
 cansperpack = int(input("How many cans are there in one pack? "))
 packs = int(input("How many packs are there? "))
 print("There are", cansperpack * packs, "cans in total.")
+print()
 
 # 3. Volume of a Rectangular Prism
 length = float(input("What is the length of the rectangular prism? "))
 width = float(input("What is the width? "))
 height = float(input("What is the height? "))
 print("The volume of the rectangular prism is", length * width * height, "units cubed.")
+print()
 
 # 4. Google Meet
 reply = input("Hey, do you just join the Google Meet and mute the teacher? ")
