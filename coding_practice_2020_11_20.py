fe65b8ace8030dbbcd325dd75902a70694c1f1c6
gavinturtl <74620025+gavinturtl@users.noreply.github.com>
2020-11-20 13:12:19 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,3 +1,8 @@
 # 1. Favourite Colour
 colour = input("What's your favourite colour? ")
 print(f"Cool! My favourite colour is {colour} too!")
+
+# 2. Packs of Cans
+cansperpack = int(input("How many cans are there in one pack? "))
+packs = int(input("How many packs are there? "))
+print("There are", cansperpack * packs, "cans in total.")
