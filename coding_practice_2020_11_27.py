e68cce29d96f81efc108a072f7b39f03ca1aa162
gavinturtl <74620025+gavinturtl@users.noreply.github.com>
2020-11-30 10:19:35 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,64 @@
+# Exercises
+# ---------
+
+# Create a while loop that will print:
+
+#     1. from 8 to 744 by 16.
+i = 8
+while i <= 744:
+    print(i)
+    i += 16
+
+#     2. from -117 to 273 by 13.
+i = -117
+while i <= 273:
+    print(i)
+    i += 13
+
+#     3. from 489 to 1306 by 19.
+i = 489
+while i <= 1306:
+    print(i)
+    i += 19
+
+#     4. from -320 to -184 by 4.
+i = -320
+while i <= -184:
+    print(i)
+    i += 4
+
+#     5. from 238 to 714 by 17.
+i = 238
+while i <= 714:
+    print(i)
+    i += 17
+
+#     6. from 228 to 874 by 17.
+i = 228
+while i <= 874:
+    print(i)
+    i += 17
+
+#     7. from 331 to 661 by 10.
+i = 331
+while i <= 661:
+    print(i)
+    i += 10
+
+#     8. from -380 to -198 by 14.
+i = -380
+while i <= -198:
+    print(i)
+    i += 14
+
+#     9. from -39 to 61 by 5.
+i = -39
+while i <= 61:
+    print(i)
+    i += 5
+
+#     10. from 327 to 435 by 6.
+i = 327
+while i <= 435:
+    print(i)
+    i += 6
