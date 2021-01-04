a96eedff3e6c50e13a7326da10b1eab92dfbeca9
gavinturtl <74620025+gavinturtl@users.noreply.github.com>
2021-01-04 09:37:50 -0500

    Create TPW_Functions_81_to_85.py

---
+++ TPW_Functions_81_to_85.py
@@ -0,0 +1,103 @@
+import math
+
+# Exercise 81:
+def main():
+    side_1 = float(input("Enter the length of the first side: "))
+    side_2 = float(input("Enter the length of the second side: "))
+    
+    print(f"The length of the hypotenuse is: {calc_hypotenuse(side_1, side_2)} units.")
+
+
+def calc_hypotenuse(side_1: float, side_2: float):
+    return math.sqrt((side_1 ** 2) + (side_2 ** 2))
+
+
+if __name__ == "__main__":
+    main()
+
+# Exercise 82:
+def main():
+    distance = float(input("Enter the distance travelled (km): "))
+
+    print(f"The total fare is ${total_fare(distance)}.")
+
+def total_fare(distance: float):
+    return 4 + ((distance / 140) * 0.25)
+
+if __name__ == "__main__":
+    main()
+
+# Exercise 83:
+def main():
+    num_of_items = int(input("Enter the number of items ordered: "))
+
+    print(f"The shipping charge is: ${shipping_charge(num_of_items)}")
+
+def shipping_charge(num_of_items: int):
+    if num_of_items < 1:
+        return 0
+    else:
+        return 10.95 + (2.95 * (num_of_items - 1))
+
+if __name__ == "__main__":
+    main()
+
+# Exercise 84:
+def main():
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    c = float(input("Enter the third number: "))
+
+    print(f"The median of the three numbers is: {median_three(a, b, c)}")
+
+def median_three(a: float, b: float, c: float):
+    if b < a and c > a or b > a and c < a:
+        return a
+    elif a < b and c > b or a > b and c < b:
+        return b
+    else:
+        return c
+
+if __name__ == "__main__":
+    main()
+
+
+# Exercise 85:
+def main():
+    int_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    for i in int_range:
+        integer = int_range[i - 1]
+        print(ordinal_num(integer))
+
+def ordinal_num(integer: int):
+    if integer < 1 or integer > 12:
+        return ""
+    else:
+        if integer == 1:
+            return "first"
+        elif integer == 2:
+            return "second"
+        elif integer == 3:
+            return "third"
+        elif integer == 4:
+            return "fourth"
+        elif integer == 5:
+            return "fifth"
+        elif integer == 6:
+            return "sixth"
+        elif integer == 7:
+            return "seventh"
+        elif integer == 8:
+            return "eighth"
+        elif integer == 9:
+            return "ninth"
+        elif integer == 10:
+            return "tenth"
+        elif integer == 11:
+            return "eleventh"
+        elif integer == 12:
+            return "twelfth"
+
+if __name__ == "__main__":
+    main()
